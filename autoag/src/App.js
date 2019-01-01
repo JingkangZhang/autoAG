2e97bff741ffdfe9172be04de541e5b2eb743ab4
Jingkang Zhang <zjk@berkeley.edu>
2019-01-01 17:02:15 +0800

    website

--- autoag/src/App.js
+++ autoag/src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and sav
           </p>
           <a
             className="App-link"
@@ -17,7 +16,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Reac
           </a>
         </header>
       </div>
