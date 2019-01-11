91fb779c77f266cb81e080f14adbdb5679860a3d
Jingkang Zhang <zjk@berkeley.edu>
2019-01-12 00:30:39 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f3974152bfeb15f7735913dbfb8d8dd.js"
+  "./precache-manifest.e85d203804819ddee455258350711915.js"
 );
 
 workbox.clientsClaim();
