b441d77cfb36efa436ff94646ac3574cde124218
Jingkang Zhang <zjk@berkeley.edu>
2020-01-01 21:06:08 +0800

    feat(TopNav): error displaying

--- src/services/index.js
+++ src/services/index.js
@@ -1,34 +1,36 @@
-const axios = require("axios").default;
+/* eslint-disable no-throw-literal */
+const axios = require('axios').default;
 
 const URL = {
   uploadAutograder:
-    "https://bdi091mwm2.execute-api.us-west-1.amazonaws.com/prod/uploadAutograder",
+    'https://bdi091mwm2.execute-api.us-west-1.amazonaws.com/prod/uploadAutograder',
   uploadSolution:
-    "https://bdi091mwm2.execute-api.us-west-1.amazonaws.com/prod/uploadSolution"
+    'https://bdi091mwm2.execute-api.us-west-1.amazonaws.com/prod/uploadSolution',
 };
 
-export const uploadAutograder = async (name, body) => {
-  try {
-    const response = await axios({
-      method: "post",
-      url: URL.uploadAutograder,
-      params: {
-        name
-      },
-      data: body
-    });
-    return response.data.data.homeworkId;
-  } catch (err) {
-    return err;
-  }
-};
+export const uploadAutograder = (name, body) => new Promise((resolve, reject) => {
+  axios({
+    method: 'post',
+    url: URL.uploadAutograder,
+    params: {
+      name,
+    },
+    data: body,
+  }).then((response) => resolve(response.data.data.homeworkId)).catch((error) => {
+    if (error.response) {
+      reject(`${error.response.status}: ${error.response.data.error}`);
+    } else {
+      reject(error.message);
+    }
+  });
+});
 
-export const uploadSolution = homeworkId => {
+export const uploadSolution = (homeworkId) => {
   axios
     .post(URL.uploadSolution, {
-      homeworkId
+      homeworkId,
     })
-    .then(response => {
+    .then((response) => {
       console.log(response);
 
       return response.data;
