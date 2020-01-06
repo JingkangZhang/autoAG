dda96e02e274c8cfb25386d6caa14a0c0533256f
Jingkang Zhang <zjk@berkeley.edu>
2020-01-06 11:02:52 +0800

    comments

--- src/containers/SubmitSolution/index.jsx
+++ src/containers/SubmitSolution/index.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import {
-  Navbar, NavbarBrand, NavItem, NavLink, InputGroup, InputGroupAddon, Input, Button, Form, FormGroup, Label, Table,
+  Navbar, NavbarBrand, NavItem, NavLink, InputGroup, InputGroupAddon, Input, Button, Form, FormGroup, Label, Table, Alert,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import Pagination from 'react-js-pagination';
 
+const PER_PAGE = 10;
+
 const SubmitSolution = () => {
-  const [test, setTest] = useState();
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+
+  // const handleChangePage = (newPageNumber) => {
+  //   const newContent = post(page=newPageNumber, perPage=PER_PAGE);
+  //   setContent(newContent);
+  //   setPage(page);
+  // }
 
   return (
     <div>
@@ -27,6 +36,7 @@ const SubmitSolution = () => {
           <Button>Search</Button>
         </div>
         <div className="submit-table">
+          {/* <Table content={content}> */}
           <Table className="submit-table-table">
             <thead>
               <tr>
@@ -52,15 +62,17 @@ const SubmitSolution = () => {
               <td>asdgasdgfhgoiuehrighjeriohgoisrhgoi</td>
               <td>asdgasdgfhgoiuehrighjeriohgoisrhgoi</td>
               <td>asdgasdgfhgoiuehrighjeriohgoisrhgoi</td>
+
             </tr>
+
           </Table>
 
         </div>
         <Pagination
-          activePage={3}
+          activePage={page}
           itemsCountPerPage={10}
           totalItemsCount={450}
-          pageRangeDisplayed={5}
+          pageRangeDisplayed={3}
           onChange={() => {}}
         />
 
