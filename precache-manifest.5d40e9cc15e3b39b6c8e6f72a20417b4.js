8790712f324cb6dec01eef08c40562966ad227a3
Jingkang Zhang <zjk@berkeley.edu>
2020-01-16 10:10:29 -0600

    Updates

---
+++ precache-manifest.5d40e9cc15e3b39b6c8e6f72a20417b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "331bf7a5496b039a9471",
+    "url": "./static/css/main.c6277412.chunk.css"
+  },
+  {
+    "revision": "331bf7a5496b039a9471",
+    "url": "./static/js/main.331bf7a5.chunk.js"
+  },
+  {
+    "revision": "be729790eb68bd3c537d",
+    "url": "./static/css/1.29154509.chunk.css"
+  },
+  {
+    "revision": "be729790eb68bd3c537d",
+    "url": "./static/js/1.be729790.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b52db9d4910a04b59d5ae1e44f5120db",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.546b25413e84acfad2be800bcac01b92.js"
+  "./precache-manifest.5d40e9cc15e3b39b6c8e6f72a20417b4.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
